2baa97a222c42dd3fa796f69c42391993bb48d07
Richard Walker <richard.walker.90@gmail.com>
2018-06-14 12:36:47 -0400

    initial commit

---
+++ src/main/java/io/richie/App.java
@@ -0,0 +1,13 @@
+package io.richie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
